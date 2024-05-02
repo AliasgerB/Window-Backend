9347a6fc0c283f27c4d3903d61834ac33e6771e9
AliasgerB <bagaliasger53@gmail.com>
2024-05-02 23:30:06 +0530

    working commit

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
+//mongodb+srv://bagaliasger53:Ali@1409@cluster0.vqhmem6.mongodb.net/AliDB
 
+//mongodb+srv://rahuljainkgk:A6DVldfcOs1ahyB1@cluster0.cei7xy2.mongodb.net/
 mongoose
-  .connect("mongodb://localhost:27017/dataneuron")
+  .connect(
+    "mongodb+srv://rahuljainkgk:A6DVldfcOs1ahyB1@cluster0.cei7xy2.mongodb.net/aliDb"
+  )
   .then(() => {
     console.log("connection succsessful...");
   })
