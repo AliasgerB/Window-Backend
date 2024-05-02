8223426ceb0f65e50f3f117c9dd62ba46546729e
AliasgerB <bagaliasger53@gmail.com>
2024-05-03 00:42:49 +0530

    change

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://rahuljainkgk:A6DVldfcOs1ahyB1@cluster0.cei7xy2.mongodb.net/aliDb"
+    "mongodb+srv://bagaliasger53:swo6nEZvJODQinv9@cluster0.q3eb7yv.mongodb.net/testthird?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("connection successful...");
