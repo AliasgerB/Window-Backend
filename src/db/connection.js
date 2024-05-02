476179c15a2a159a40320d65adfa6a8c15e50628
AliasgerB <bagaliasger53@gmail.com>
2024-05-02 23:51:34 +0530

    redeploy

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 //mongodb+srv://bagaliasger53:Ali@1409@cluster0.vqhmem6.mongodb.net/AliDB
 
+// mongo atlas client
 //mongodb+srv://rahuljainkgk:A6DVldfcOs1ahyB1@cluster0.cei7xy2.mongodb.net/
 mongoose
   .connect(
