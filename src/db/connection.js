20e06796ef87fade85e0cb7e0828e08a6de5cfb0
AliasgerB <bagaliasger53@gmail.com>
2024-05-03 00:02:10 +0530

    comment code remove

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
-//mongodb+srv://bagaliasger53:Ali@1409@cluster0.vqhmem6.mongodb.net/AliDB
 
-// mongo atlas client
-//mongodb+srv://rahuljainkgk:A6DVldfcOs1ahyB1@cluster0.cei7xy2.mongodb.net/
 mongoose
   .connect(
     "mongodb+srv://rahuljainkgk:A6DVldfcOs1ahyB1@cluster0.cei7xy2.mongodb.net/aliDb"
