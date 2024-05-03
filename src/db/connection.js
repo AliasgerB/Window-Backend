d337817b1b54514fc8cc71765d575562233cc78f
AliasgerB <bagaliasger53@gmail.com>
2024-05-03 13:09:25 +0530

    some changes

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-// "mongodb+srv://rahuljainkgk:A6DVldfcOs1ahyB1@cluster0.cei7xy2.mongodb.net/aliDb"
 
 mongoose
   .connect(process.env.MONGODB_URI)
